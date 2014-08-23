0abe0be8f2f5dc49847a909f812ac37b8a5805e7
Ryan Thompson <service@aiwebsystems.com>
2014-08-22 20:17:12 -0500

    Initial commit.

---
+++ src/TextFieldType.php
@@ -0,0 +1,40 @@
+<?php namespace Addon\FieldType\Text;
+
+use Streams\Core\Addon\FieldTypeAbstract;
+
+class TextFieldType extends FieldTypeAbstract
+{
+    /**
+     * The database column type this field type uses.
+     *
+     * @var string
+     */
+    public $columnType = 'string';
+
+    /**
+     * Field type version
+     *
+     * @var string
+     */
+    public $version = '1.1.0';
+
+    /**
+     * Field type author information.
+     *
+     * @var array
+     */
+    public $author = array(
+        'name' => 'AI Web Systems, Inc.',
+        'url'  => 'http://aiwebsystems.com/',
+    );
+
+    /**
+     * Return a new presenter instance.
+     * @param $resource
+     * @return TextFieldTypePresenter|\Streams\Presenter\AddonPresenter|\Streams\Presenter\FieldTypePresenter|\Streams\Presenter\PresenterAbstract
+     */
+    public function newPresenter($resource)
+    {
+        return new TextFieldTypePresenter($resource);
+    }
+}
---
+++ src/TextFieldTypePresenter.php
@@ -0,0 +1,11 @@
+<?php namespace Addon\FieldType\Text;
+
+use Streams\Core\Addon\Presenter\FieldTypePresenter;
+
+class TextFieldTypePresenter extends FieldTypePresenter
+{
+    public function count()
+    {
+        return strlen($this->resource->value()) . ' = strlen("' . $this->resource->value() . '")';
+    }
+}
