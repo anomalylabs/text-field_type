40e73255a7c9b5a57db1855608511f74412cf2cc
Ryan Thompson <service@aiwebsystems.com>
2014-10-21 12:11:22 -0500

    Restored from Timemachine

--- src/TextFieldType.php
+++ src/TextFieldType.php
@@ -4,4 +4,12 @@ use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
 class TextFieldType extends FieldTypeAddon
 {
+    protected $slug = 'text';
+
+    /**
+     * The database column type this field type uses.
+     *
+     * @var string
+     */
+    public $columnType = 'string';
 }
