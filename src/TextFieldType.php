27f4367db62582c40d49bd48f11b38086fce2450
Ryan Thompson <service@aiwebsystems.com>
2014-11-04 01:17:06 -0600

    Doc'd

--- src/TextFieldType.php
+++ src/TextFieldType.php
@@ -2,6 +2,15 @@
 
 use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
+/**
+ * Class TextFieldType
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Addon\FieldType\Text
+ */
 class TextFieldType extends FieldTypeAddon
 {
+    // Ya..
 }
