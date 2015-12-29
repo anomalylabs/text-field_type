e0905a686273a6148aca0a7519f9fbe3f7eca357
Ryan Thompson <service@aiwebsystems.com>
2015-12-29 01:41:28 -0600

    Max length indicator

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,40 +1,54 @@
 $(function () {
 
     // Initialize text suggestions
-    $('.text-field-type input').each(function () {
+    $('input[data-provides="text"]').each(function () {
 
-        $(this).textcomplete([
-            {
-                tags: ['{{ entry.last_name }}', '{{ forms_display($slug) }}'],
-                match: /{{2}\s[a-z_.]+(?!}})$/,
-                search: function (term, callback) {
-                    callback($.map(this.tags, function (tag) {
-                        return tag.indexOf(term) === 0 ? tag : null;
-                    }));
-                },
-                index: 0,
-                replace: function (tag) {
-                    return tag;
-                }
+        var wrapper = $(this).closest('div');
+        var counter = wrapper.find('.counter');
+
+        $(this).characterCounter({
+            limit: $(this).data('max'),
+            counterSelector: $(this).closest('div').find('.count'),
+            onExceed: function () {
+                counter.addClass('text-danger');
             },
-            {
-                tags: [
-                    '{input.subject}',
-                    '{input.email}',
-                    '{input.phone}',
-                    '{input.name}'
-                ],
-                match: /{{1}[a-z_.]+(?!}})$/,
-                search: function (term, callback) {
-                    callback($.map(this.tags, function (tag) {
-                        return tag.indexOf(term) === 0 ? tag : null;
-                    }));
-                },
-                index: 0,
-                replace: function (tag) {
-                    return tag;
-                }
+            onDeceed: function () {
+                counter.removeClass('text-danger');
             }
-        ]);
+        });
+
+        /*$(this).textcomplete([
+         {
+         tags: ['{{ entry.last_name }}', '{{ forms_display($slug) }}'],
+         match: /{{2}\s[a-z_.]+(?!}})$/,
+         search: function (term, callback) {
+         callback($.map(this.tags, function (tag) {
+         return tag.indexOf(term) === 0 ? tag : null;
+         }));
+         },
+         index: 0,
+         replace: function (tag) {
+         return tag;
+         }
+         },
+         {
+         tags: [
+         '{input.subject}',
+         '{input.email}',
+         '{input.phone}',
+         '{input.name}'
+         ],
+         match: /{{1}[a-z_.]+(?!}})$/,
+         search: function (term, callback) {
+         callback($.map(this.tags, function (tag) {
+         return tag.indexOf(term) === 0 ? tag : null;
+         }));
+         },
+         index: 0,
+         replace: function (tag) {
+         return tag;
+         }
+         }
+         ]);*/
     });
 });
