92a326215cdede8606d81673b34dd65d3db15b2a
Ryan Thompson <ryan@pyrocms.com>
2020-04-09 12:22:01 -0500

    Vuetify

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -1,1 +1,23 @@
-<input {!! html_attributes($fieldType->attributes()) !!}>
+<v-input>
+    <v-text-field
+    {!! html_attributes([
+        'value' => $fieldType->getValue(),
+        'label' => $fieldType->getLabel(),
+        'name'  => $fieldType->getInputName(),
+        'hint'  =>  $fieldType->getInstructions(),
+        
+        'readonly' => $fieldType->isReadonly() ? true : false,
+        'disabled' => $fieldType->isDisabled() ? true : false,
+    
+        // 'data-field'      => $fieldType->getField(),
+        // 'data-provides'   => $fieldType->getNamespace(),
+        // 'data-field_name' => $fieldType->getFieldName(),
+    
+        // 'class' => $fieldType->getClass(),
+        // 'id'    => $fieldType->getInputName(),
+    ]) !!}
+    
+    outlined
+    clearable
+    ></v-text-field>
+</v-input>
