1c632ce8e1144fdcde5466b55a2fbc6460c2c67d
Ryan Thompson <service@aiwebsystems.com>
2020-04-20 23:20:02 -0500

    Wait..

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -1,23 +1,15 @@
-<v-input>
-    <v-text-field
-    {!! html_attributes([
-        'value' => $fieldType->getValue(),
-        'label' => $fieldType->getLabel(),
-        'name'  => $fieldType->getInputName(),
-        'hint'  =>  $fieldType->getInstructions(),
-        
-        'readonly' => $fieldType->isReadonly() ? true : false,
-        'disabled' => $fieldType->isDisabled() ? true : false,
-    
-        // 'data-field'      => $fieldType->getField(),
-        // 'data-provides'   => $fieldType->getNamespace(),
-        // 'data-field_name' => $fieldType->getFieldName(),
-    
-        // 'class' => $fieldType->getClass(),
-        // 'id'    => $fieldType->getInputName(),
-    ]) !!}
-    
-    outlined
-    clearable
-    ></v-text-field>
-</v-input>
+<input autocomplete="off" {!! html_attributes($fieldType->attributes([
+    'value'       => $fieldType->getValue(),
+    'name'        => $fieldType->getInputName(),
+    'placeholder' => $fieldType->getPlaceholder(),
+
+    'readonly' => $fieldType->isReadonly() ? 'readonly' : '',
+    'disabled' => $fieldType->isDisabled() ? 'disabled' : '',
+
+    'data-field'      => $fieldType->getField(),
+    'data-provides'   => $fieldType->getNamespace(),
+    'data-field_name' => $fieldType->getFieldName(),
+
+    'class' => $fieldType->getClass() ?: 'input',
+    'id'    => $fieldType->getInputName(),
+])) !!}>
