5c5c98799e46f91a333c26d0ac856c0187840f91
Ryan Thompson <service@aiwebsystems.com>
2020-04-21 00:07:04 -0500

    Auto

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -1,15 +1,1 @@
-<input autocomplete="off" {!! html_attributes($fieldType->attributes([
-    'value'       => $fieldType->getValue(),
-    'name'        => $fieldType->getInputName(),
-    'placeholder' => $fieldType->getPlaceholder(),
-
-    'readonly' => $fieldType->isReadonly() ? 'readonly' : '',
-    'disabled' => $fieldType->isDisabled() ? 'disabled' : '',
-
-    'data-field'      => $fieldType->getField(),
-    'data-provides'   => $fieldType->getNamespace(),
-    'data-field_name' => $fieldType->getFieldName(),
-
-    'class' => $fieldType->getClass() ?: 'input',
-    'id'    => $fieldType->getInputName(),
-])) !!}>
+<input autocomplete="off" {!! html_attributes($fieldType->attributes()) !!}>
